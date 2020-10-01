20b0ee4309056a444545855f049be33716043791
Melissa <melissakheying@gmail.com>
2020-10-01 10:51:43 -0500

    :lipstick: style client card

--- client/src/components/guests/Client.js
+++ client/src/components/guests/Client.js
@@ -1,35 +1,31 @@
 import React from 'react'
+import './client-card.css'
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
 // adding font-awesome icons is tricky
 // faEdit for example, looks like fa-edit in fontawesome docs. you have to convert it to camelCase
 // solid, brands, regular are collections of icons ex: "@fortawesome/free-solid-svg-icons";
-library.add(fas, faEdit, faTrash, faPhone);
+library.add(fas, faEdit, faTrash);
 
 export const Client = () => {
   return (
-    <div className="box">
-    <div className="">
-      <div>
-        <button className="button mr-4 pl-4 pr-4">
-          <FontAwesomeIcon icon="edit" className="icon" />
+    <div className='card-container container'>
+    <img alt='photo' className='image' src='https://source.unsplash.com/random' />
+        <h1 className='title is-2'>name</h1>
+        <p>phone</p>
+        <p>email</p>
+        <div className=''>
+        <button>
+        <FontAwesomeIcon icon="edit" className="icon"/>
         </button>
-        <button className="button mr-4 pl-4 pr-4">
-          <FontAwesomeIcon icon="trash" className="icon" />
+        <button>
+        <FontAwesomeIcon icon="trash" className="icon"/>
         </button>
-      </div>
-    </div>
-    <div className="">
-      <h2>Name</h2>
-        <FontAwesomeIcon icon="phone" className="icon" />
-        <i className="fas fa-phone" />
-        <p>111-111-1111</p>
-      </div>
+        </div>
     </div>
   )
 }
