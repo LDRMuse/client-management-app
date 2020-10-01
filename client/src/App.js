5ca991a37fe0076ccbc6ac086adc34981ca581a4
Melissa <melissakheying@gmail.com>
2020-10-01 11:44:36 -0500

    wrap everything in ClientState so that every component works

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Dashboard } from './components'
+import { ClientState } from './context/clientContext/ClientState'
 
 function App() {
   return (
-    <div className="App">
-    <h1 className='title is-3'>app</h1>
-    <Dashboard />
-    </div>
+    <ClientState>
+      <div className="App">
+        <h1 className='title is-3'>app</h1>
+        <Dashboard />
+      </div>
+    </ClientState>
+
   );
 }
 
