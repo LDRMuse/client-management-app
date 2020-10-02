56dfc041e6cadbc1de49657d38bf5fae0520e2b1
Melissa <melissakheying@gmail.com>
2020-10-02 12:41:00 -0500

    :sparkles: 1st function 2 get value of inputs, 2nd function 2 use values
    2nd function only logs values currently

--- client/src/components/guests/ClientForm/ClientForm.js
+++ client/src/components/guests/ClientForm/ClientForm.js
@@ -3,9 +3,11 @@ import React, { Fragment, useContext, useState } from 'react'
 import { ChartEntry } from './ChartEntry'
 import { ClientState } from '../../../context/clientContext/ClientState';
 
-
+//TODO add a client and display on page
+// edit guest table?
 export const ClientForm = () => {
   // const { addClient } = useContext(ClientState)
+
 
   const [client, setClient] = useState({
     firstName: "",
@@ -13,28 +15,43 @@ export const ClientForm = () => {
     email: "",
     phone: "",
   })
+  // this is just destructing client so we can use as the value on the form
+  const { firstName, lastName, email, phone } = client
 
+  // first we have to set the values in the input
+  const handleChange = (e) => {
+    e.preventDefault()
+    setClient({
+      ...client,
+      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value,
+    })
+  }
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault()
-  //   addClient(client)
-  //   setClient({
-  //     firstName: "",
-  //     lastName: "",
-  //     email: "",
-  //     phone: "",
-  //   })
-  // }
-  // console.log(client, 'hiii')
+  //second, we need to do something with the values
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(client)
+    // set the client back to empty strings
+    setClient({
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: ""
+    })
+  }
+
 
   return (
     <Fragment>
-      <form onSubmit=''>
+      <form onSubmit={handleSubmit}>
         <h1 className='title is-3'>Client Form</h1>
-        <input type="text" placeholder="First Name" name="firstName" value={client.firstName} onChange='' />
-        <input type="text" placeholder="Last Name" name="phone" value={client.lastName} onChange='' />
-        <input type="text" placeholder="email" name="email" value={client.email} onChange='' />
-        <input type="text" placeholder="Phone" name="phone" value={client.phone} onChange='' />
+        <input type="text" placeholder="First Name" name="firstName" value={firstName} onChange={handleChange} />
+        <input type="text" placeholder="Last Name" name="lastName" value={lastName} onChange={handleChange} />
+        <input type="text" placeholder="email" name="email" value={email} onChange={handleChange} />
+        <input type="text" placeholder="Phone" name="phone" value={phone} onChange={handleChange} />
         <button
           className="button is-primary ml-3 mt-2"
           type="submit"
