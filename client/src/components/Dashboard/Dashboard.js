4aa20043ff88fa50483339881192aec80bc08d5f
Melissa <melissakheying@gmail.com>
2020-10-02 09:35:54 -0500

    :bug: fix:tada: buttons display certain component when button is clicked

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -3,39 +3,29 @@ import { Clients, ClientForm } from '../guests'
 
 export const Dashboard = () => {
   const [clicked, setClicked] = useState(false)
+  const [add, setAdd] = useState(false)
 
-  const handleClick = () => {
-    switch (clicked) {
-      case 'View/SearchClients':
-        setClicked(true)
-        break
-      case 'Add New Client':
-        setClicked(true)
-        break
-      default: setClicked(false)
+
+  const handleClick = (e) => {
+    if (e.currentTarget.value === 'View') {
+      setClicked(true)
+      setAdd(false)
+    } else if (e.currentTarget.value === 'Add') {
+      setAdd(true)
+      setClicked(false)
     }
-
   }
-  const buttonChoices = ['View/SearchClients', 'Add New Client'].map(buttonId => (
-    <button
-      className='button mb-5'
-      key={buttonId}
-      value={buttonId}
-      onClick={handleClick}
-    >
-      {buttonId}
-    </button>
-  ))
 
-//TODO figure out how to conditionally display components when the button type is clicked
+
   return (
     <Fragment>
       <div className='container box'>
         <h1 className='title is-3'>Hi, User!</h1>
         <div className='container has-text-centered'>
-          {buttonChoices}
-          {clicked && 'View/SearchClients' ? <Clients /> : null}
-          {clicked && 'Add New Client' ? <ClientForm /> : null}
+          <button className='button' value='View' onClick={handleClick}>View/Search Clients</button>
+          {clicked ? <Clients /> : null}
+          <button className='button' value='Add' onClick={handleClick}>Add New Client</button>
+          {add ? <ClientForm /> : null}
         </div>
       </div>
     </Fragment>
